092b38dba5c6a6834a5eda72de57f1d985a7798d
cloud2003 <alena.cloud@gmail.com>
2017-06-13 20:07:46 +0300

    HM 8. Not realization drag&drop

--- src/index.js
+++ src/index.js
@@ -1,26 +1,99 @@
 var myFriends = document.querySelector('#myFriends');
-var addButton = document.querySelector('#myFriends .b-list__plus');
 var addedFriends = document.querySelector('#addedFriends');
-var deleteButton = document.querySelector('#addedFriends .b-list__plus');
-var filterNameInput = document.querySelector('#filterNameInput');
-var loadListOfFriends;
+var buttonSafe = document.querySelector('#buttonSafe');
+var filterInput = document.querySelectorAll('.filter');
+let listOfFriends = [];
+let vkRequest;
 
-var myModule = {
+var vkModule = {
     Init: function() {
-        loadListOfFriends = document.querySelectorAll('.b-list__item');
 
-        //типо фильтрация
-        filterNameInput.addEventListener('keyup', function(e) {
-            loadListOfFriends.forEach(function (person) {
-                if ( !isMatching(person.querySelector('.b-list__name').innerText, e.target.value) && e.target.value != '' ) {
-                    person.style.display = 'none';
-                } else {
-                    person.style = '';
+        function filterName(event) {
+            var list = event.dataset.list;
+            var listArr;
+
+            if (list == 'myFriends') {
+                listArr = vkRequest.items;
+            } else {
+                if (list == 'addedFriends') {
+                    listArr = listOfFriends;
                 }
+            }
+
+            if (event.value != '') {
+                var newListFilter = listArr.filter(function (person) {
+                    if ( isMatching(person.first_name, event.value) || isMatching(person.last_name, event.value) ) {
+                        return true;
+                    }
+                });
+
+                createFriendsList(document.getElementById(list), newListFilter);
+            } else {
+                createFriendsList(document.getElementById(list), listArr);
+            }
+        }
+
+        filterInput.forEach(function (el) {
+            el.addEventListener('keyup', function (e) {
+                filterName(e.target);
             });
+        });
+
+        // Save in localStorage
+        buttonSafe.addEventListener('click', function () {
+            localStorage.setItem('listOfFriends', JSON.stringify(listOfFriends));
+            localStorage.setItem('vkRequest', JSON.stringify(vkRequest));
+        });
+
+        // addButton
+        myFriends.addEventListener('click', function (e) {
+            if (e.target.className == 'b-list__plus') {
+                vkRequest.items.filter(function(person, i) {
+                    if (e.path[0].id == person.id) {
+                        listOfFriends.push(person);
+                        vkRequest.items.splice(i, 1);
+                    }
+                });
+                // list rendering
+                createFriendsList(addedFriends, listOfFriends);
+                createFriendsList(myFriends, vkRequest.items);
+            }
+            filterName(document.querySelector('#filterAllFriends'));
+        });
+
+        // deleteButton
+        addedFriends.addEventListener('click', function (e) {
+            if (e.target.className == 'b-list__plus') {
+                listOfFriends.filter(function(person, i) {
+                    if (e.path[0].id == person.id) {
+                        vkRequest.items.push(person);
+                        listOfFriends.splice(i, 1);
+                    }
+                });
+
+                // list rendering
+                createFriendsList(addedFriends, listOfFriends);
+                createFriendsList(myFriends, vkRequest.items);
+            }
+            filterName(document.querySelector('#filterSelectedFriends'));
         });
     }
 };
+
+function createFriendsList(where, obj) {
+    where.innerHTML = templateFn(obj);
+}
+
+function isMatching (full, chunk) {
+    full = full.toLowerCase();
+    chunk = chunk.toLowerCase();
+
+    if ( ~full.indexOf(chunk) && chunk.length != 0 ) {
+        return true;
+    }
+
+    return false;
+}
 
 function vkApi(method, options) {
     return new Promise(function (resolve, reject) {
@@ -34,56 +107,40 @@ function vkApi(method, options) {
     });
 }
 
-function createFriendsList(obj) {
-    //console.log(obj);
-    var html = templateFn(obj);
-    myFriends.innerHTML = html;
-}
-
-function isMatching (full, chunk) {
-    // console.log('full, chunk');
-    // console.log(full, chunk);
-    full = full.toLowerCase();
-    chunk = chunk.toLowerCase();
-
-    if ( ~full.indexOf(chunk) && chunk.length != 0 ) {
-        return true;
-    }
-
-    return false;
-}
-
 var template = `
-    {{#each items}}
-    <div class="b-list__item" id="{{id}}">
-        <div class="b-list__plus"></div>
+    {{#each []}}
+    <div class="b-list__item">
+        <div class="b-list__plus" id="{{id}}"></div>
         <div class="b-list__img"><img src="{{#with photo_100}}{{this}}{{else}}img/no-image.gif{{/with}}" alt="{{first_name}} {{last_name}}" /></div>
         <div class="b-list__name">{{first_name}} {{last_name}}</div>
     </div>
     {{/each}}`;
 var templateFn = Handlebars.compile(template);
 
+new Promise(function(resolve, reject) {
+    VK.init({
+        apiId: 6056913
+    });
 
-new Promise(function (resolve) {
-        window.onload = resolve();
+    VK.Auth.login(function(response) {
+        if (response.session) {
+            resolve(response);
+        } else {
+            reject(new Error('Пользователь нажал кнопку Отмена в окне авторизации'));
+        }
+    }, 2);
+})
+    .then(() => vkApi('friends.get', { v: '5.64', 'fields': 'country, city, photo_100' }))
+    .then(function(response) {
+        if (window.localStorage.hasOwnProperty('listOfFriends') && window.localStorage.hasOwnProperty('vkRequest')) {
+            vkRequest = JSON.parse(localStorage.getItem('vkRequest'));
+            listOfFriends = JSON.parse(localStorage.getItem('listOfFriends'));
+            createFriendsList(myFriends, vkRequest.items);
+            createFriendsList(addedFriends, listOfFriends);
+        } else {
+            createFriendsList(myFriends, response.response.items);
+            vkRequest = response.response;
+        }
     })
-    .then(() => {
-        return new Promise(function (resolve, reject) {
-            VK.init({
-                apiId: 6056913
-            });
-
-            VK.Auth.login(function(response) {
-                if (response.session) {
-                    resolve(response);
-                } else {
-                    reject(new Error('Пользователь нажал кнопку Отмена в окне авторизации'));
-                }
-            }, 2);
-        });
-    })
-    .then(() => vkApi('friends.get', {v: '5.64', 'fields' : 'country, city, photo_100'}))
-    .then(responce => createFriendsList(responce.response))
-    .then(() => myModule.Init())
+    .then(() => vkModule.Init())
     .catch(e => alert('Ошибка ' + e.message));
-
