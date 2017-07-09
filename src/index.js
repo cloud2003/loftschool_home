6da6427302c4bad5fd4d6de0214389ddc3c6167c
cloud2003 <alena.cloud@gmail.com>
2017-07-09 13:58:41 +0300

    HM 9. YandexMap

--- src/index.js
+++ src/index.js
@@ -1,212 +1,210 @@
 var myPoints = [];
+var myReviews = [];
+var myIdPoint = 0;
+var placemarks = [];
+
 
 ymaps.ready(init);
 
+
 function init() {
-    // Создание экземпляра карты и его привязка к созданному контейнеру.
-    var myMap = new ymaps.Map('map', {
-            center: [55.751574, 37.573856],
-            zoom: 15,
-            behaviors: ['default', 'scrollZoom']
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
+  // Создание экземпляра карты и его привязка к созданному контейнеру.
+  var myMap = new ymaps.Map('map', {
+      center: [55.777777, 37.577777],
+      zoom: 15,
+      behaviors: ['default', 'scrollZoom']
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
 
-        // Создание макета балуна на основе Twitter Bootstrap.
-        MyBalloonLayout = ymaps.templateLayoutFactory.createClass(
-            '<div class="popover top">' +
-            '<a class="close" href="#">&times;</a>' +
-            '<div class="arrow"></div>' +
-            '<div class="popover-inner">' +
-            '$[[options.contentLayout observeSize minWidth=235 maxWidth=335 maxHeight=350]]' +
-            '</div>' +
-            '</div>', {
-                /**
-                 * Строит экземпляр макета на основе шаблона и добавляет его в родительский HTML-элемент.
-                 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/layout.templateBased.Base.xml#build
-                 * @function
-                 * @name build
-                 */
-                build: function () {
-                    this.constructor.superclass.build.call(this);
+    // Создаем собственный макет с информацией о выбранном геообъекте.
+    customItemContentLayout = ymaps.templateLayoutFactory.createClass(
+      // Флаг "raw" означает, что данные вставляют "как есть" без экранирования html.
+      '<h2 class=ballon_header>{{ properties._data.balloonHeader|raw }}</h2>' +
+      '<div class=ballon_body>{{ properties._data.name|raw }}</div>' +
+      '<div class=ballon_footer>{{ properties._data.place|raw }}</div>' +
+      '<div class=ballon_footer>{{ properties._data.text|raw }}</div>'
+    ),
 
-                    this._$element = $('.popover', this.getParentElement());
+    // Создание экземпляра кластера
+    clusterer = new ymaps.Clusterer({
+      preset: 'islands#invertedVioletClusterIcons',
+      // Устанавливаем стандартный макет балуна кластера "Карусель".
+      clusterBalloonContentLayout: 'cluster#balloonCarousel',
+      // Устанавливаем собственный макет.
+      clusterBalloonItemContentLayout: customItemContentLayout,
+      groupByCoordinates: false,
+      clusterDisableClickZoom: true,
+      clusterHideIconOnBalloonOpen: false,
+      geoObjectHideIconOnBalloonOpen: false
+    }),
 
-                    this.applyElementOffset();
+    // Создание макета балуна на основе Twitter Bootstrap.
+    MyBalloonLayout = ymaps.templateLayoutFactory.createClass(
+      '<div class="popover top">' +
+        '<a class="close" href="#">&times;</a>' +
+        '<div class="popover-inner">' +
 
-                    this._$element.find('.close')
-                        .on('click', $.proxy(this.onCloseClick, this));
-                },
+          '<h3 class="popover-title">{{ balloonHeader }}</h3>' +
+          '<div class="popover-content">' +
+            '<div class="popover-review">' +
+              '{{ balloonContent }} \
+              {% for review in reviews %} \
+                <div class="popover-review__item">\
+                  <strong>{{ review.name }}</strong>, <span>{{ review.place }}</span> \
+                  <p>{{ review.text }}</p> \
+                </div>\
+              {% endfor %} \
+            </div>' +
+            '<h4>Ваш отзыв</h4>' +
+            '<form>' +
+              '<input type="text" id="inputName" class="popover-name form-control form-control-sm" placeholder="Введите имя" /><br />' +
+              '<input type="text" id="inputPlace" class="popover-place form-control form-control-sm" placeholder="Укажите место" /><br />' +
+              '<textarea name="" id="textareaReview" cols="10" rows="5" class="form-control form-control-sm"></textarea><br />' +
+            '</form>' +
+            '<button id="popover-button" class="btn btn-warning">Добавить</button>' +
+          '</div>' +
+        '</div>' +
+      '</div>', {
+        // Строит экземпляр макета на основе шаблона и добавляет его в родительский HTML-элемент.
+        build: function () {
+          this.constructor.superclass.build.call(this);
+          this._$element = $('.popover', this.getParentElement());
+          this._$element.find('.close').on('click', $.proxy(this.onCloseClick, this));
+          //this._$element.find('#popover-button').on('click', $.proxy(this.onAddReview, this));
 
-                /**
-                 * Удаляет содержимое макета из DOM.
-                 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/layout.templateBased.Base.xml#clear
-                 * @function
-                 * @name clear
-                 */
-                clear: function () {
-                    this._$element.find('.close')
-                        .off('click');
+          // var inputName = document.querySelector('#inputName');
+          // var inputPlace = document.querySelector('#inputPlace');
+          // var textareaReview = document.querySelector('#textareaReview');
 
-                    this.constructor.superclass.clear.call(this);
-                },
+          // console.log(this);
+          // console.log(this._$element);
+        },
+        // Добавляем отзыв.
+        onAddReview: function (e) {
+          if (inputName.value != '' && inputPlace.value != '' && textareaReview.value != '') {
+            myPoints.push({
+              id: myIdPoint,
+              name: inputName.value,
+              place: inputPlace.value,
+              text: textareaReview.value,
+              //address:
+            });
 
-                /**
-                 * Метод будет вызван системой шаблонов АПИ при изменении размеров вложенного макета.
-                 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/IBalloonLayout.xml#event-userclose
-                 * @function
-                 * @name onSublayoutSizeChange
-                 */
-                onSublayoutSizeChange: function () {
-                    MyBalloonLayout.superclass.onSublayoutSizeChange.apply(this, arguments);
+            var myReviewsText = '';
+            myReviewsText += myPoints[myPoints.length - 1].name + ' - ' + myPoints[myPoints.length - 1].place + ' - ' + myPoints[myPoints.length - 1].text + '<hr/>';
 
-                    if(!this._isElement(this._$element)) {
-                        return;
-                    }
+            inputName.value = inputPlace.value = textareaReview.value = '';
+          }
+        },
+        onCloseClick: function (e) {
+          e.preventDefault();
+          this.events.fire('userclose');
+        }
+      });
 
-                    this.applyElementOffset();
+  // Создание независимого экземпляра балуна и отображение его в центре карты.
+  var balloon = new ymaps.Balloon(myMap, {layout: MyBalloonLayout});
+  // Здесь родительскими устанавливаются опции карты, где содержатся значения по умолчанию для обязательных опций.
+  balloon.options.setParent(myMap.options);
 
-                    this.events.fire('shapechange');
-                },
+  balloon.events.add('click', function (e) {
+    // console.log('domEvent -->'); console.log(domEl);
 
-                /**
-                 * Сдвигаем балун, чтобы "хвостик" указывал на точку привязки.
-                 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/IBalloonLayout.xml#event-userclose
-                 * @function
-                 * @name applyElementOffset
-                 */
-                applyElementOffset: function () {
-                    this._$element.css({
-                        left: -(this._$element[0].offsetWidth / 2),
-                        top: -(this._$element[0].offsetHeight + this._$element.find('.arrow')[0].offsetHeight)
-                    });
-                },
+    if (e.get('domEvent').get('target').id == 'popover-button') {
+      // берем координаты точки
+      var coords = e.get('target').getPosition();
 
-                /**
-                 * Закрывает балун при клике на крестик, кидая событие "userclose" на макете.
-                 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/IBalloonLayout.xml#event-userclose
-                 * @function
-                 * @name onCloseClick
-                 */
-                onCloseClick: function (e) {
-                    e.preventDefault();
+      // console.log(e.get('target')._data.balloonHeader);
+      // console.log(coords.join(','));
 
-                    this.events.fire('userclose');
-                },
+      if (inputName.value != '' && inputPlace.value != '' && textareaReview.value != '') {
 
-                /**
-                 * Используется для автопозиционирования (balloonAutoPan).
-                 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/ILayout.xml#getClientBounds
-                 * @function
-                 * @name getClientBounds
-                 * @returns {Number[][]} Координаты левого верхнего и правого нижнего углов шаблона относительно точки привязки.
-                 */
-                getShape: function () {
-                    if(!this._isElement(this._$element)) {
-                        return MyBalloonLayout.superclass.getShape.call(this);
-                    }
+        var myPoint = {
+          id: myIdPoint,
+          coords: coords,
+          name: inputName.value,
+          place: inputPlace.value,
+          text: textareaReview.value,
+          balloonHeader: e.get('target')._data.balloonHeader
+        };
 
-                    var position = this._$element.position();
+        myPoints.push(myPoint);
 
-                    return new ymaps.shape.Rectangle(new ymaps.geometry.pixel.Rectangle([
-                        [position.left, position.top], [
-                            position.left + this._$element[0].offsetWidth,
-                            position.top + this._$element[0].offsetHeight + this._$element.find('.arrow')[0].offsetHeight
-                        ]
-                    ]));
-                },
+        // console.log('myPoints', myPoints);
+        console.log('myPoint', myPoint);
 
-                /**
-                 * Проверяем наличие элемента (в ИЕ и Опере его еще может не быть).
-                 * @function
-                 * @private
-                 * @name _isElement
-                 * @param {jQuery} [element] Элемент.
-                 * @returns {Boolean} Флаг наличия.
-                 */
-                _isElement: function (element) {
-                    return element && element[0] && element.find('.arrow')[0];
-                }
-            }),
+        inputName.value = inputPlace.value = textareaReview.value = '';
+        ++myIdPoint;
 
-        // Создание вложенного макета содержимого балуна.
-        MyBalloonContentLayout = ymaps.templateLayoutFactory.createClass(
-            '<h3 class="popover-title">$[properties.balloonHeader]</h3>' +
-            '<div class="popover-content">$[properties.balloonContent]</div>' +
-            '<div class="popover-content">' +
-            '<input type="text" id="inputName" class="popover-name" placeholder="Имя" /><br />' +
-            '<input type="text" id="inputPlace" class="popover-place" placeholder="Место" /><br />' +
-            '<textarea name="" id="textareaReview" cols="10" rows="5"></textarea><br />' +
-            '</div>' +
-            '<button id="popover-button">Добавить</button>'
-        );
+        // создаем Placemark
+        var placemark = new ymaps.Placemark(coords, myPoint);
+        // myMap.geoObjects.add(placemark);
 
-    var inputName = document.querySelector('#inputName');
-    var inputPlace = document.querySelector('#inputPlace');
-    var textareaReview = document.querySelector('#textareaReview');
+        // добавляем метку в массив
+        placemarks.push(placemark);
 
-    // Слушаем клик на карте.
-    myMap.events.add('click', function (e) {
-        var idClick = 0,
-            coords = e.get('coords');
+        console.log('placemarks', placemarks);
 
-        myPoints.push({
-            id: idClick++,
-            coords: coords
+        // добавляем метки на карту
+        clusterer.add(placemarks);
+        myMap.geoObjects.add(clusterer);
+
+
+        /*var newText = '';
+        newText += templateFn(getReviewsByCoords(coords));*/
+
+        // переоткрываем balloon
+        balloon.open(coords, {
+          balloonHeader: myPoint.balloonHeader,
+          balloonContent: '',
+          reviews: getReviewsByCoords(coords)
+          // balloonContent: newText
         });
+        console.log();
+      }
+    }
+    // Добавляем кластер на карту
+    myMap.geoObjects.add(clusterer);
 
-        console.log(myPoints);
+  });
 
-        var myPlacemark = window.myPlacemark = createPlacemark(coords);
-        console.log(myPlacemark);
-        myMap.geoObjects.add(myPlacemark);
-        myPlacemark.balloon.open();
+  // Слушаем клик на карте.
+  myMap.events.add('click', function (e) {
 
-        getAddress(coords);
+    var coords = e.get('coords');
+
+    ymaps.geocode(coords).then(function (res) {
+      var firstGeoObject = res.geoObjects.get(0);
+      var address = firstGeoObject.getAddressLine();
+      balloon.open(coords, {
+        balloonHeader: address,
+        balloonContent: 'Пока еще нет отзывов'
+      });
     });
-
-    // Создание метки.
-    function createPlacemark(coords) {
-        return new ymaps.Placemark(coords, {
-            iconCaption: 'поиск...',
-            //balloonHeader: 'Заголовок балуна',
-            //balloonContent: 'Контент балуна'
-        }, {
-            //preset: 'islands#violetDotIconWithCaption',
-            //draggable: false,
-            balloonLayout: MyBalloonLayout,
-            balloonContentLayout: MyBalloonContentLayout,
-            // Запретим замену обычного балуна на балун-панель.
-            // Если не указывать эту опцию, на картах маленького размера откроется балун-панель.
-            balloonPanelMaxMapArea: 0
-            // Не скрываем иконку при открытом балуне.
-            // hideIconOnBalloonOpen: false,
-            // И дополнительно смещаем балун, для открытия над иконкой.
-            // balloonOffset: [3, -40]
+  });
+  
+  // Отзывы по координатам
+  function getReviewsByCoords(coords) {
+    var reviews = [];
+    for (let i = 0; i < myPoints.length; i++) {
+      if (coords.join(',') == myPoints[i].coords.join(',')) {
+        reviews.push({
+          name: myPoints[i].name,
+          place: myPoints[i].place,
+          text: myPoints[i].text
         });
+      }
     }
+    return reviews;
+  }
 
-    // Определяем адрес по координатам (обратное геокодирование).
-    function getAddress(coords) {
-        myPlacemark.properties.set('iconCaption', 'поиск...');
-        ymaps.geocode(coords).then(function (res) {
-            var firstGeoObject = res.geoObjects.get(0);
-
-            myPlacemark.properties
-                .set({
-                    /*// Формируем строку с данными об объекте.
-                     iconCaption: [
-                     // Название населенного пункта или вышестоящее административно-территориальное образование.
-                     firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
-                     // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
-                     firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
-                     ].filter(Boolean).join(', '),*/
-                    // В качестве контента балуна задаем строку с адресом объекта.
-                    balloonHeader: firstGeoObject.getAddressLine(),
-                    balloonContent: 'dfsdfsd'
-                });
-        });
-    }
-
+  /*var template = `
+    {{#each []}}
+    <div class="b-list__item">
+        <strong class="b-name">{{name}}</strong> <span class="b-place">{{place}}</span>
+        <p>{{ text }}</p>
+    </div>
+    {{/each}}`;
+  var templateFn = Handlebars.compile(template);*/
 }
-
